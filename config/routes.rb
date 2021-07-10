46aa739b642e760b2af16062113003267b6c295e
itonaga-takao <itonaga124@yahoo.co.jp>
2021-07-11 00:16:09 +0900

    ユーザー管理機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'items/index'
+  devise_for :users
+  root to: 'items#index'
+  # resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
+     
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
